bc4afa6db41cb7e3cf5611f36958a51ee8ddf510
Zeno <zeno.hamers@gmail.com>
2024-04-12 15:30:33 +0200

    Initial setup langchain + openAI

---
+++ langchain_setup.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+llm = ChatOpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "{input}")
+])
+output_parser = StrOutputParser()
+
+
+chain = prompt | llm | output_parser
+
+output = chain.invoke({"input": "how can langsmith help with testing?"})
+
+print(output)
