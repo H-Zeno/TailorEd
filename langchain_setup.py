4f100155e0568d982a5fc722fc2d24312862b6aa
Zeno <zeno.hamers@gmail.com>
2024-04-12 16:15:25 +0200

    simple embedding working

--- langchain_setup.py
+++ langchain_setup.py
@@ -3,22 +3,35 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import CharacterTextSplitter
+
 
 
 load_dotenv()
 
 llm = ChatOpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+embeddings = OpenAIEmbeddings()
 
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
-output_parser = StrOutputParser()
+loader = TextLoader(".\Content\Oxford_English_Dictionary.txt", encoding="utf-8")
+
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=20)
+docs = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(docs, embeddings)
+print(db.index.ntotal)
 
 
-chain = prompt | llm | output_parser
+query = "What has four legs, is in the house and you can put things on?"
+docs = db.similarity_search(query)
 
-output = chain.invoke({"input": "how can langsmith help with testing?"})
 
-print(output)
+
+
+
+
+
